f24696b89f27b73aabfbf1a59b38991d154e2e76
浜田　俊祐 <hamashun0820@icloud.com>
2024-02-19 16:37:27 +0900

    add: 逆ポーランド記法への変換を追加

---
+++ conv.go
@@ -0,0 +1,55 @@
+package main
+
+import "strings"
+
+var precedence = map[string]int{
+	"+": 1,
+	"-": 1,
+	"*": 2,
+	"/": 2,
+	"^": 3,
+}
+
+func stringToTokens(s string) []string {
+	tokens := make([]string, 0)
+	for _, r := range strings.Split(s, " ") {
+		if r == " " {
+			continue
+		}
+		tokens = append(tokens, string(r))
+	}
+	return tokens
+}
+
+func ConvRPN(s string) *Queue[string] {
+	tokens := stringToTokens(s)
+	stack := NewStack[string]()
+	que := NewQueue[string]()
+
+	for _, token := range tokens {
+		if token == "+" || token == "-" || token == "*" || token == "/" || token == "^" {
+			for !stack.IsEmpty() && precedence[stack.Top()] >= precedence[token] {
+				que.Push(stack.Top())
+				stack.Pop()
+			}
+			stack.Push(token)
+		} else if token == "(" {
+			stack.Push(token)
+		} else if token == ")" {
+			for stack.Top() != "(" {
+				que.Push(stack.Top())
+				stack.Pop()
+			}
+			stack.Pop()
+		} else {
+			que.Push(token)
+		}
+	}
+
+	for !stack.IsEmpty() {
+		que.Push(stack.Top())
+		stack.Pop()
+	}
+
+	return que
+}
---
+++ conv_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestStringToTokens(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "1 + 21 * 33",
+			args: args{s: "1 + 21 * 33"},
+			want: []string{"1", "+", "21", "*", "33"},
+		},
+		{
+			name: "1 + 21 * 33 - 4",
+			args: args{s: "1 + 21 * 33 - 4"},
+			want: []string{"1", "+", "21", "*", "33", "-", "4"},
+		},
+		{
+			name: "1 + 21 * 33 - 4 / 5",
+			args: args{s: "1 + 21 * 33 - 4 / 5"},
+			want: []string{"1", "+", "21", "*", "33", "-", "4", "/", "5"},
+		},
+		{
+			name: "4 ^ 3 + 21 * 33 - 4 / 5 + 6",
+			args: args{s: "4 ^ 3 + 21 * 33 - 4 / 5 + 6"},
+			want: []string{"4", "^", "3", "+", "21", "*", "33", "-", "4", "/", "5", "+", "6"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := stringToTokens(tt.args.s)
+			if len(got) != len(tt.want) {
+				t.Errorf("stringToTokens() = %v, want %v", got, tt.want)
+				return
+			}
+			for i, v := range got {
+				if v != tt.want[i] {
+					t.Errorf("stringToTokens() = %v, want %v", got, tt.want)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestConvRPN(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "1 + 21 * 33",
+			args: args{s: "1 + 21 * 33"},
+			want: "[1 21 33 * +]",
+		},
+		{
+			name: "1 + 21 * 33 - 4",
+			args: args{s: "1 + 21 * 33 - 4"},
+			want: "[1 21 33 * + 4 -]",
+		},
+		{
+			name: "1 + 21 * 33 - 4 / 5",
+			args: args{s: "1 + 21 * 33 - 4 / 5"},
+			want: "[1 21 33 * + 4 5 / -]",
+		},
+		{
+			name: "4 ^ 3 + 21 * 33 - 4 / 5 + 6",
+			args: args{s: "4 ^ 3 + 21 * 33 - 4 / 5 + 6"},
+			want: "[4 3 ^ 21 33 * + 4 5 / - 6 +]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConvRPN(tt.args.s)
+			if got.String() != tt.want {
+				t.Errorf("ConvRPN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
