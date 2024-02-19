151052c0745ca3046d344192af855eb4421b89a1
浜田　俊祐 <hamashun0820@icloud.com>
2024-02-19 22:05:22 +0900

    add: 入力を受け付けるようにした

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for {
+		fmt.Print("input formula > ")
+		// python input() equivalent in Go
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		// remove newline character
+		input = input[:len(input)-1]
+		fmt.Println("You entered: ", input)
+		rpn := ConvRPN(input)
+		fmt.Println("RPN: ", rpn)
+		result := Calc(rpn)
+		fmt.Println("Result: ", result)
+		fmt.Println()
+	}
+}
